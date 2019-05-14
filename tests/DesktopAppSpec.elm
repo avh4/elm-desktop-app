4d108ee2dbcefb416d5b6492131c326a0f5d88df
Aaron VonderHaar <gruen0aermel@gmail.com>
2019-05-13 21:04:47 -0700

    Backfill test for loading initial data

--- tests/DesktopAppSpec.elm
+++ tests/DesktopAppSpec.elm
@@ -4,20 +4,48 @@ import DesktopApp.Testable as DesktopApp
 import Expect exposing (Expectation)
 import Html
 import Test exposing (..)
-import TestContext exposing (TestContext)
+import Test.Html.Selector exposing (text)
+import TestContext
 
 
-start : TestContext () () ( Cmd (), List DesktopApp.Effect )
+type alias TestModel =
+    { count : Int }
+
+
+type TestMsg
+    = NoOp
+    | Loaded Int
+
+
+type alias TestContext =
+    TestContext.TestContext TestMsg TestModel ( Cmd TestMsg, List DesktopApp.Effect )
+
+
+start : TestContext
 start =
     let
         program =
             DesktopApp.program
-                { init = ( (), Cmd.none )
-                , subscriptions = \() -> Sub.none
-                , update = \() () -> ( (), Cmd.none )
-                , view = \() -> Html.text ""
-                , files = DesktopApp.jsonFile "data.json" identity (DesktopApp.jsonMapping ())
-                , noOp = ()
+                { init = ( { count = 0 }, Cmd.none )
+                , subscriptions = \model -> Sub.none
+                , update =
+                    \msg model ->
+                        case msg of
+                            NoOp ->
+                                ( model, Cmd.none )
+
+                            Loaded newCount ->
+                                ( { model | count = newCount }
+                                , Cmd.none
+                                )
+                , view = \model -> Html.text ("count:" ++ String.fromInt model.count)
+                , files =
+                    DesktopApp.jsonFile "data.json"
+                        identity
+                        (DesktopApp.jsonMapping Loaded
+                            |> DesktopApp.withInt "count" .count
+                        )
+                , noOp = NoOp
                 }
     in
     TestContext.create
@@ -33,5 +61,18 @@ all =
         [ test "loads persisted state" <|
             \() ->
                 start
-                    |> TestContext.done
+                    |> simulateLoadFile "data.json" """{"count":7}"""
+                    |> TestContext.expectViewHas [ text "count:7" ]
         ]
+
+
+simulateLoadFile :
+    String
+    -> String
+    -> TestContext.TestContext TestMsg model ( cmd, List DesktopApp.Effect )
+    -> TestContext.TestContext TestMsg model ( cmd, List DesktopApp.Effect )
+simulateLoadFile expectedFilename loadedContent testContext =
+    testContext
+        |> TestContext.shouldHaveLastEffect (Tuple.second >> Expect.equal [ DesktopApp.LoadFile expectedFilename ])
+        -- TODO: Avoid manually creating the msg after https://github.com/avh4/elm-program-test/issues/17 is implemented
+        |> TestContext.update (Loaded 7)
